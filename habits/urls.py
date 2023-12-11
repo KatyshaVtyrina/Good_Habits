217c07374b9c50416522b8040b6ffcf6c204fef9
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-11 21:52:15 +0300

    implementation of crud for habit

--- habits/urls.py
+++ habits/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
 from habits.apps import HabitsConfig
+from habits.views import HabitCreateAPIView, HabitListAPIView, HabitDetailAPIView, HabitUpdateAPIView, HabitDeletePIView
 
 app_name = HabitsConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path('', HabitListAPIView.as_view(), name='habits_list'),
+    path('create/', HabitCreateAPIView.as_view(), name='create_habit'),
+    path('<int:pk>/', HabitDetailAPIView.as_view(), name='habit'),
+    path('update/<int:pk>/', HabitUpdateAPIView.as_view(), name='update_habit'),
+    path('delete/<int:pk>/', HabitDeletePIView.as_view(), name='delete_habit'),
+]
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    """Создание привычки"""
+    serializer_class = HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+    """Просмотр всех привычек"""
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitDetailAPIView(generics.RetrieveAPIView):
+    """Просмотр детальной информации о привычке"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    """Редактирование привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitDeletePIView(generics.DestroyAPIView):
+    """Удаление привычки"""
+    queryset = Habit.objects.all()
