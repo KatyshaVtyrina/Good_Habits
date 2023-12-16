5f92c0f29e6a761e40caf7d3e74fe3becb0fe48c
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-16 20:28:19 +0300

    add permissions, automatic saving of the user when creating a habit; redefinition queryset in HabitListAPIView

--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from habits.models import Habit
+from habits.permissions import IsUser
 from habits.serializers import HabitSerializer
 from paginators import HabitPaginator
 
@@ -8,13 +10,26 @@ from paginators import HabitPaginator
 class HabitCreateAPIView(generics.CreateAPIView):
     """Создание привычки"""
     serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """Автоматическое сохранение владельца при создании объекта"""
+        serializer.save(user=self.request.user)
 
 
 class HabitListAPIView(generics.ListAPIView):
-    """Просмотр всех привычек"""
+    """Просмотр привычек"""
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
     pagination_class = HabitPaginator
+
+    def get_queryset(self):
+        """Если модератор - возвращается список всех привычек, если нет - список привычек
+        авторизованного пользователя"""
+        if self.request.user.is_authenticated:
+            if self.request.user.is_staff:
+                return self.queryset
+            return self.queryset.filter(user=self.request.user)
 
 
 class HabitPublicListAPIView(generics.ListAPIView):
@@ -22,20 +37,24 @@ class HabitPublicListAPIView(generics.ListAPIView):
     queryset = Habit.objects.all().filter(is_public=True)
     serializer_class = HabitSerializer
     pagination_class = HabitPaginator
+    permission_classes = [IsAuthenticated]
 
 
 class HabitDetailAPIView(generics.RetrieveAPIView):
     """Просмотр детальной информации о привычке"""
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsUser]
 
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
     """Редактирование привычки"""
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsUser]
 
 
 class HabitDeletePIView(generics.DestroyAPIView):
     """Удаление привычки"""
     queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsUser]
