9ffff95e1d04e5e89f4faaf2877b846850a8e838
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-16 13:03:22 +0300

    add validators for Habit

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
 
 from habits.models import Habit
+from habits.validators import *
 
 
 class HabitSerializer(serializers.ModelSerializer):
-    """Класс-сериализатор для создания привычки"""
+    """Класс-сериализатор для модели Habit"""
 
     class Meta:
         model = Habit
         fields = '__all__'
+
+    def validate(self, data):
+        """
+        Валидаторы для модели Habit.
+        """
+        validate_related_habit_or_reward(data)
+        validate_time_to_complete(data)
+        validate_related_habit_is_pleasant(data)
+        validate_is_pleasant(data)
+        validate_periodicity(data)
+        return data
