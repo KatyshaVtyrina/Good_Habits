88479a12ef796dd05bd2b9b54a5f972ea04ec7e7
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-17 13:28:14 +0300

    install celery, celery-beat

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'drf_yasg',
     'corsheaders',
+    'django_celery_beat',
 
     'habits',
     'users',
@@ -159,3 +160,8 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:8000']
 CORS_ALLOW_ALL_ORIGINS = False
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+
+CELERY_BEAT_SCHEDULE = {}
