daf73de53c6763801820cb24266f91af1cce13a8
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-18 21:48:31 +0300

    add instance in validate_related_habit_or_reward, validate_is_pleasant,

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 
 from habits.models import Habit
-from habits.validators import *
+
+from habits.validators import (validate_related_habit_or_reward, validate_time_to_complete,
+                               validate_related_habit_is_pleasant, validate_is_pleasant,
+                               validate_periodicity)
 
 
 class HabitSerializer(serializers.ModelSerializer):
@@ -15,9 +18,9 @@ class HabitSerializer(serializers.ModelSerializer):
         """
         Валидаторы для модели Habit.
         """
-        validate_related_habit_or_reward(data)
+        validate_related_habit_or_reward(data, self.instance)
         validate_time_to_complete(data)
         validate_related_habit_is_pleasant(data)
-        validate_is_pleasant(data)
+        validate_is_pleasant(data, self.instance)
         validate_periodicity(data)
         return data
--- habits/validators.py
+++ habits/validators.py
@@ -3,16 +3,24 @@ import datetime
 from rest_framework.serializers import ValidationError
 
 
-def validate_related_habit_or_reward(value):
+def validate_related_habit_or_reward(value, instance):
     """Исключает одновременное указание связанной привычки и вознаграждения"""
-    if value.get('related_habit') and value.get('reward'):
+    if instance is not None:
+        related_habit = value.get('related_habit', instance.related_habit)
+        reward = value.get('reward', instance.reward)
+    else:
+        related_habit = value.get('related_habit')
+        reward = value.get('reward')
+
+    if related_habit and reward:
         raise ValidationError("Нельзя указывать одновременно связанную привычку и вознаграждение")
 
 
 def validate_time_to_complete(value):
     """Проверяет время выполнения привычки. Не может быть более двух минут"""
-    if value.get('time_to_complete') > datetime.time(hour=0, minute=2, second=0):
-        raise ValidationError("Время выполнения не может быть более двух минут")
+    if value.get("time_to_complete"):
+        if value.get('time_to_complete') > datetime.time(hour=0, minute=2, second=0):
+            raise ValidationError("Время выполнения не может быть более двух минут")
 
 
 def validate_related_habit_is_pleasant(value):
@@ -21,14 +29,23 @@ def validate_related_habit_is_pleasant(value):
         raise ValidationError("В связанные привычки могут попадать только привычки с признаком приятной привычки.")
 
 
-def validate_is_pleasant(value):
+def validate_is_pleasant(value, instance):
     """Проверка наличия вознаграждения и/или связанной привычки у приятной привычки"""
-    if value.get('is_pleasant'):
-        if value.get('related_habit') or value.get('reward'):
+    if instance is not None:
+        is_pleasant = value.get('is_pleasant', instance.is_pleasant)
+        related_habit = value.get('related_habit', instance.related_habit)
+        reward = value.get('reward', instance.reward)
+    else:
+        is_pleasant = value.get('is_pleasant')
+        related_habit = value.get('related_habit')
+        reward = value.get('reward')
+
+    if is_pleasant:
+        if related_habit or reward:
             raise ValidationError('У приятной привычки не может быть вознаграждения или связанной привычки.')
 
 
 def validate_periodicity(value):
     """Проверка периодичности, не может быть реже, чем 1 раз в 7 дней."""
-    if value.get('periodicity') < 7:
+    if value.get('periodicity') and value.get('periodicity') > 7:
         raise ValidationError('Нельзя выполнять привычку реже, чем 1 раз в 7 дней.')
