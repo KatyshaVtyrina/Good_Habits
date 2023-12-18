071ec63161e49e41cd2d494aaa203d7fea7da17f
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-18 21:44:27 +0300

    add settings for Telegram, Сelery

--- config/settings.py
+++ config/settings.py
@@ -163,7 +163,10 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
-
-CELERY_BEAT_SCHEDULE = {}
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 TELEGRAM_BOT_API_KEY = os.getenv('TELEGRAM_BOT_API_KEY')
+TELEGRAM_URL = 'https://api.telegram.org/bot'
