8663a51e8b4ad07492c8ff740eb8f93499c199ee
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-20 20:02:30 +0300

    change class Reminder on func create_reminder

--- habits/services.py
+++ habits/services.py
@@ -1,29 +1,38 @@
+from datetime import datetime, timedelta
+
 from habits.models import Habit
 import json
 
-
 from django_celery_beat.models import PeriodicTask, \
-    IntervalSchedule
+    IntervalSchedule, CrontabSchedule
 
 
-class Reminder:
-    """Класс, описывающий расписание отправки напоминаний о привычке"""
+# class Reminder:
+#     """Класс, описывающий расписание отправки напоминаний о привычке"""
+#
+#     def __init__(self, habit: Habit):
+#         self.habit = habit
 
-    def __init__(self, habit: Habit):
-        self.habit = habit
+def create_reminder(habit):
+    # Создаем расписание
+    # schedule, created = IntervalSchedule.objects.get_or_create(
+    #     every=habit.periodicity,
+    #     period=IntervalSchedule.DAYS,
+    # )
 
-    def create_reminder(self):
+    schedule, created = CrontabSchedule.objects.get_or_create(
+        minute=habit.time.minute,
+        hour=habit.time.hour,
+        day_of_month=f'*/{habit.periodicity}',
+        month_of_year='*',
+        day_of_week='*',
+    )
 
-        # Создаем расписание
-        schedule, created = IntervalSchedule.objects.get_or_create(
-            every=self.habit.periodicity,
-            period=IntervalSchedule.DAYS,
-        )
-
-        # Создаем задачу для повторения
-        PeriodicTask.objects.create(
-            interval=schedule,
-            name=f'send_telegram_message_{self.habit.id}',
-            task='habits.tasks.send_telegram_message',
-            args=json.dumps([self.habit.id]),
-        )
+    # Создаем задачу для повторения
+    PeriodicTask.objects.create(
+        crontab=schedule,
+        name=f'send_telegram_message_{habit.id}',
+        task='habits.tasks.send_telegram_message',
+        args=[habit.id],
+        # kwargs=json.dumps({'habit_id': self.habit.id}),
+    )
