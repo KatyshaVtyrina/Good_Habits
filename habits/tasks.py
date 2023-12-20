58cddecaa5c689825ce0b0c0aef31c88dbebcc23
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-20 20:01:06 +0300

    correction task

--- habits/tasks.py
+++ habits/tasks.py
@@ -5,15 +5,16 @@ from config import settings
 from habits.models import Habit
 
 
-@shared_task()
+@shared_task
 def send_telegram_message(habit_id):
-    habit = Habit.objects.get(id=habit_id)
+    # print(habit_id[0])
+    habit = Habit.objects.get(pk=habit_id)
 
     requests.post(
         url=f'{settings.TELEGRAM_URL}{settings.TELEGRAM_BOT_API_KEY}/sendMessage',
         data={
             'chat_id': habit.user.tg_id,
             'text': f'Пора выполнить привычку {habit.action}, место выполнения - {habit.place}, '
-                    f'время выполнения - {habit.time_to_complete}'
+                    f'время выполнения - {habit.time}'
         }
     )
