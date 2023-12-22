38fe806a403c4fd1bb3b155b7613739dc24b714a
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-22 17:50:47 +0300

    add field 'id' in HabitAdmin

--- habits/admin.py
+++ habits/admin.py
@@ -5,7 +5,7 @@ from habits.models import Habit
 
 @admin.register(Habit)
 class HabitAdmin(admin.ModelAdmin):
-    list_display = ('user', 'place', 'time', 'action', 'is_pleasant', 'related_habit', 'periodicity', 'reward',
+    list_display = ('id', 'user', 'place', 'time', 'action', 'is_pleasant', 'related_habit', 'periodicity', 'reward',
                     'time_to_complete', 'is_public')
     search_fields = ('user', 'place', 'action',)
     list_filter = ('user', 'is_public')
